53a127dc197b58df8775b653f45b520e672bd5da
hafsairfan888 <hafsairfan888@gmail.com>
2024-11-24 18:48:44 +0500

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -27,15 +27,6 @@ function Home() {
   return (
     <div className="homepage--container">
       <Navbar />
-      <div className="booksbox">{bookElements}</div>
-      <div className="challenge">
-        <h3 className="challenge--heading">Reading Challenges & Lists</h3>
-        <ul className="challenge--list">
-          <li><a href="https://app.thestorygraph.com/reading_challenges/75592277-0f6e-438e-ba65-0ce6c43088d8" target="_blank" rel="noopener noreferrer">BBC Top 100 Books (C)</a></li>
-          <li><a href="https://www.goodreads.com/list/show/541.Best_Twists" target="_blank" rel="noopener noreferrer">Must Read Thrillers (L)</a></li>
-          <li><a href="https://www.goodreads.com/list/show/740.I_Should_Probably_Read_This_Sometime_" target="_blank" rel="noopener noreferrer">Classics to Read (L)</a></li>
-        </ul>
-      </div>
       <div className="wordscloud"></div>
       <ButtonToTop />
       <Footer />
