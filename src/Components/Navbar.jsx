7ff6247d9c6899dafc182a81b48ede33b12a9127
hafsairfan888 <hafsairfan888@gmail.com>
2024-11-24 23:53:58 +0500

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,12 +9,9 @@ export default function Navbar() {
       <div className="homepage--texts">
       <h1 className="nav--text">WELCOME TO HGS LIBRARY!</h1>
       <p className="nav--para">
-        Here, you'll find a vast collection of books, inspiring authors, 
-        and thought-provoking quotes, all designed to enrich your learning journey. 
-        Whether you're looking for academic resources or motivational reads, our eLibrary 
-        has something for every student. Explore, learn, and let your imagination soar!
+        Here, you'll find a vast collection of books, inspiring authors, and thought-provoking quotes, all designed to enrich your learning journey. Whether you're looking for academic resources or motivational reads, our Digital Library has something for every student. Dive into the world of knowledge and discover new ideas to broaden your horizons. With endless opportunities to explore and grow, your next great adventure starts here!
       </p>
-      <p className="nav--subpara">Coded with hate, love & pani puri by Nagraj</p>
+      <p className="nav--subpara">Developed By Hafsa Irfan</p>
       </div>
       <div className="lottie--container">
         <Lottie animationData={books} />
