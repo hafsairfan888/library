8b63931bd8a80339fbca0de687701c2f25d1aea9
hafsairfan888 <hafsairfan888@gmail.com>
2024-11-28 21:36:03 +0500

    Update AuthorsData.jsx

--- src/data/AuthorsData.jsx
+++ src/data/AuthorsData.jsx
@@ -1896,7 +1896,7 @@ const authorsData = [
         id: 1,
         img: thistimeitsreal,
         title: "This Time It's Real",
-        value: "This Time It's Real",
+        value: "This Time Its Real",
         author: "Ann Liang",
         genre: "Contemporary",
         pages: 352,
