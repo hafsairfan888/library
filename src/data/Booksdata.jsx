d3753f343c48bfb86fb801949fd129a886010292
hafsairfan888 <hafsairfan888@gmail.com>
2024-11-29 11:39:40 +0500

    Update Booksdata.jsx

--- src/data/Booksdata.jsx
+++ src/data/Booksdata.jsx
@@ -1457,7 +1457,7 @@ export default [
     id:122,
     img: thistimeitsreal,
     title: "This Time It's Real",
-    value: "This Time Its Real",
+    value: "This time its real",
     author: "Ann Liang",
     genre: "Contemporary",
     pages: 352,
